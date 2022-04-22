096d7e2bf7def92c240a63ee1a37d366831dd976
mcelhatton <colinm@mobiledealerdata.com>
2022-04-22 13:14:42 -0500

    saving progress

--- app.js
+++ app.js
@@ -1,34 +1,88 @@
-const fs = require('fs');
-const math = require('./math');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-console.log(math.add(4,9));
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [name, github] = profileDataArgs;
-
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
-
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your github username?'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself.'
+    }
+  ])
 };
+  //.then(answers => console.log(answers));
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+  const promptProject = () => {
+    if(!portfolioData.project) {
+      portfolioData.projects = [];
+    }
+    
+    console.log(`
+    ================
+    Add a New Project
+    ================
+    `);
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)'
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to add this project?',
+        default: false
+      }
+      
+    ]);
+    
+  }
+  
 
-  console.log('Portfolio complete!  Check out index.html to see the output.');
+  promptUser()
+    .then(answers => console.log(answers))
+    .then(promptProject)
+    .then(projectAnswers => console.log(projectAnswers));
 
-});
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
